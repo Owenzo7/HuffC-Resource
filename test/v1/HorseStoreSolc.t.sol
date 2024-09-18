4a814a57551572a61f89b1226c9ff1a62a3359e3
Owen Lee <lee@Owens-MacBook-Air.local>
2024-09-18 16:43:04 +0300

    made some changes

--- test/v1/HorseStoreSolc.t.sol
+++ test/v1/HorseStoreSolc.t.sol
@@ -4,6 +4,4 @@ pragma solidity 0.8.20;
 
 import {Base_testV1} from "./Base_testV1.sol";
 
-
 contract HorseStoreSolc is Base_testV1 {}
-
---
+++ test/v1/HorseStoreYul.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.20;
+
+import {Base_testV1} from "./Base_testV1.sol";
+import {HorseStoreYul} from "../../src/horseStoreV1/HorseStoreYul.sol";
+
+contract HorseStoreSolc is Base_testV1 {
+    function setUp() public override {
+        horseStore = new HorseStoreYul();
+    }
+}
