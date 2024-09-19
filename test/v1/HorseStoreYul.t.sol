e0967b022661c2e540b115b5cacbd4bf6bb4e35d
Owen Lee <lee@Owens-MacBook-Air.local>
2024-09-19 08:29:19 +0300

    made some changes

--- test/v1/HorseStoreYul.t.sol
+++ test/v1/HorseStoreYul.t.sol
@@ -2,11 +2,13 @@
 
 pragma solidity 0.8.20;
 
+
+import {IHorseStore} from "../../src/horseStoreV1/IHorseStore";
 import {Base_testV1} from "./Base_testV1.sol";
 import {HorseStoreYul} from "../../src/horseStoreV1/HorseStoreYul.sol";
 
 contract HorseStoreSolc is Base_testV1 {
     function setUp() public override {
-        horseStore = new HorseStoreYul();
+        horseStore = new IHorseStore(HorseStoreYul());
     }
 }
